ed2112ac4ed23c7bac0d3b5666c7bae8990e61a9
modbots <77435462+modbots@users.noreply.github.com>
2023-07-30 15:31:14 +0630

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -2,7 +2,7 @@
 #relay login -k ce10e352-5cf9-4c4d-b0b7-a9834f7b74b1 -s k74jiYF1Kzo2
 echo "ttyd serving at port 80 with username:pass as kali:kali"
 ./vless.sh bash
-nohup relay connect --name xxx & ls
+nohup relay connect --name kmkl & ls
 chmod +x /usr/local/bin/ttyd_linux
 /usr/local/bin/ttyd_linux -p 80 -c kali:kali bash 
 
